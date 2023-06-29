02e47f1a8aca0bdf8d9b8733bd107d1d3dd49d92
Alena <alenabelovadmvn@gmail.com>
2023-06-29 18:00:26 -0400

    Initial Commit

---
+++ Xcode Tutorial View App/Xcode_Tutorial_View_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_View_AppApp.swift
+//  Xcode Tutorial View App
+//
+//  Created by Alena Belova  on 2023-06-29.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_View_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
